2c5365d62d17462663adced2bfe36d02af776d28
NMBM773 <138221227+NMBM773@users.noreply.github.com>
2025-11-29 23:52:58 +0800

    Add files via upload

---
+++ STM32F103/STM32F103工程模板/HARDWARE/delay/delay.h
@@ -0,0 +1,8 @@
+#include"stm32f10x.h"
+#ifndef __DELAY_H
+#define __DELAY_H
+void delay_us(u32 n);
+void delay_ms(u16 ms);
+void delay_s(u16 s);
+void delayms(u32 n);
+#endif
---
+++ STM32F103/STM32F103工程模板/USER/main.c
@@ -0,0 +1,10 @@
+# include "stm32f10x.h"
+# include "delay.h"
+#include "sys.h"
+int main(void)
+{
+	while(1)
+	{
+;
+	}		
+}
